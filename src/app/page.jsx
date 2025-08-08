00e6c4807a57f01081d3d837eef8fcca37b3c818
mahmudul-hasan01 <mahamudul2018@gmail.com>
2025-08-08 16:29:31 +0600

    create about section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Hero from "./_components/hero";
 import Cocktails from "./_components/cocktails";
+import About from "./_components/about";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -14,8 +15,8 @@ const Pome = () => {
       <div className="flex-center h-[100vh]">
         <Hero />
       </div>
-      {/* <div className="h-dvh bg-black"></div> */}
       <Cocktails />
+      <About />
     </div>
   );
 };
